7fcad0eca3627292b38fc7914f077cff4921e4af
i515142 <daniel.penchev@sap.com>
2019-07-15 16:05:19 +0300

    Refactor RPCalculationAlgorithm

--- src/test/java/calculator/computationTests/ReversePolishCalculationAlgorithmTest.java
+++ src/test/java/calculator/computationTests/ReversePolishCalculationAlgorithmTest.java
@@ -5,8 +5,8 @@ import calculator.computation.ReversePolishCalculationAlgorithm;
 
 import calculator.exceptions.InvalidOperatorException;
 import calculator.exceptions.MissingOperatorException;
-import calculator.exceptions.OutOfItemsException;
 import calculator.inputControl.EquationValidator;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -34,44 +34,37 @@ public class ReversePolishCalculationAlgorithmTest {
 
     private final double DELTA=0.0001;
 
+    @Before
+    public void setUp()
+    {
+        Mockito.when(validator.isValidNumber("2.0")).thenReturn(true);
+        Mockito.when(validator.isValidNumber("3.0")).thenReturn(true);
+        Mockito.when(validator.isValidNumber("+")).thenReturn(false);
+    }
+
 
     @Test(expected = EmptyStackException.class)
     public void calculationEquation_EquationWithMissingNumber_OutOfItemsExceptionThrown() throws Exception
     {
         Mockito.when(machine.computeAction("+",2.0,3)).thenReturn(5.0);
-
-        Mockito.when(validator.isValidNumber("2.0")).thenReturn(true);
-        Mockito.when(validator.isValidNumber("3")).thenReturn(true);
-        Mockito.when(validator.isValidNumber("+")).thenReturn(false);
-
-        algorithm.calculateEquation("2.0 3 + +".split(" "));
+        algorithm.calculateEquation("2.0 3.0 + +".split(" "));
     }
 
 
     @Test(expected = MissingOperatorException.class)
     public void calculationEquation_EquationWithMissingOperator_MissingOperatorExceptionThrown() throws Exception
     {
-        Mockito.when(machine.computeAction("+",2.0,1)).thenReturn(3.0);
-
-        Mockito.when(validator.isValidNumber("2.0")).thenReturn(true);
-        Mockito.when(validator.isValidNumber("1.0")).thenReturn(true);
-        Mockito.when(validator.isValidNumber("+")).thenReturn(false);
-        Mockito.when(validator.isValidNumber("3.0")).thenReturn(true);
-
-        algorithm.calculateEquation("2.0 1.0 + 3.0".split(" "));
+        Mockito.when(machine.computeAction("+",2.0,2.0)).thenReturn(4.0);
+        algorithm.calculateEquation("2.0 2.0 + 3.0".split(" "));
     }
 
     @Test(expected = InvalidOperatorException.class)
     public void calculationEquation_EquationWithInvalidOperator_InvalidOperatorException() throws Exception
     {
-        Mockito.when(validator.isValidNumber("2.0")).thenReturn(true);
-        Mockito.when(validator.isValidNumber("1.0")).thenReturn(true);
         Mockito.when(validator.isValidNumber("#")).thenReturn(false);
+        Mockito.when(machine.computeAction("#",2.0,3.0)).thenThrow(new InvalidOperatorException("Invalid operator"));
 
-
-        Mockito.when(machine.computeAction("#",2.0,1.0)).thenThrow(new InvalidOperatorException("Invalid operator"));
-
-        algorithm.calculateEquation("2.0 1.0 #".split(" "));
+        algorithm.calculateEquation("2.0 3.0 #".split(" "));
     }
 
     @Test
@@ -81,11 +74,8 @@ public class ReversePolishCalculationAlgorithmTest {
         Mockito.when(machine.computeAction("+",3.0,27.0)).thenReturn(30.0);
 
         Mockito.when(validator.isValidNumber("^")).thenReturn(false);
-        Mockito.when(validator.isValidNumber("3")).thenReturn(true);
-        Mockito.when(validator.isValidNumber("+")).thenReturn(false);
 
-
-        assertEquals(30,algorithm.calculateEquation("3 3 3 ^ +".split(" ")),DELTA);
+        assertEquals(30,algorithm.calculateEquation("3.0 3.0 3.0 ^ +".split(" ")),DELTA);
     }
 
 }
