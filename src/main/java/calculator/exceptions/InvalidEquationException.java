476133fd3e790550a237a20b26bdce337da9cd26
i515142 <daniel.penchev@sap.com>
2019-07-17 18:15:47 +0300

    Add InvalidEquationException

---
+++ src/main/java/calculator/exceptions/InvalidEquationException.java
@@ -0,0 +1,8 @@
+package calculator.exceptions;
+
+public class InvalidEquationException extends Exception {
+    public InvalidEquationException(final String message)
+    {
+        super(message);
+    }
+}
