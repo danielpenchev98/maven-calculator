d9d305ed978ae045b822f2029b577e4b7346135c
i515142 <daniel.penchev@sap.com>
2019-07-17 11:17:29 +0300

    Add integration tests for ReversePolishCalculationAlgorithm

---
+++ src/test/java/calculator/computationTests/ReversePolishCalculationAlgorithmIT.java
@@ -0,0 +1,57 @@
+package calculator.computationTests;
+
+import calculator.computation.ComputationalMachine;
+import calculator.computation.MathArithmeticOperatorFactory;
+import calculator.computation.ReversePolishCalculationAlgorithm;
+import calculator.exceptions.InvalidOperatorException;
+import calculator.exceptions.MissingOperatorException;
+import calculator.inputControl.EquationValidator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.*;
+
+public class ReversePolishCalculationAlgorithmIT {
+
+    private ReversePolishCalculationAlgorithm algorithm;
+
+    private final double DELTA=0.0001;
+
+    @Before
+    public void setUp()
+    {
+        ComputationalMachine machine=new ComputationalMachine(new MathArithmeticOperatorFactory());
+        EquationValidator validator=new EquationValidator();
+        algorithm=new ReversePolishCalculationAlgorithm(machine,validator);
+    }
+
+
+    @Test(expected = EmptyStackException.class)
+    public void calculationEquation_EquationWithMissingNumber_OutOfItemsExceptionThrown() throws Exception
+    {
+        algorithm.calculateEquation("2.0 3.0 + +".split(" "));
+    }
+
+
+    @Test(expected = MissingOperatorException.class)
+    public void calculationEquation_EquationWithMissingOperator_MissingOperatorExceptionThrown() throws Exception
+    {
+        algorithm.calculateEquation("2.0 2.0 + 3.0".split(" "));
+    }
+
+    @Test(expected = InvalidOperatorException.class)
+    public void calculationEquation_EquationWithInvalidOperator_InvalidOperatorException() throws Exception
+    {
+        algorithm.calculateEquation("2.0 3.0 #".split(" "));
+    }
+
+    @Test
+    public void calculationEquation_LegalEquation() throws Exception
+    {
+
+        assertEquals(30,algorithm.calculateEquation("3.0 3.0 3.0 ^ +".split(" ")),DELTA);
+    }
+
+}
