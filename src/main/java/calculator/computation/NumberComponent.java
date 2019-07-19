7b516e54062a72eefc0152a9835ffe6aaaf465fa
i515142 <daniel.penchev@sap.com>
2019-07-19 15:36:00 +0300

    Create NumberComponent

---
+++ src/main/java/calculator/computation/NumberComponent.java
@@ -0,0 +1,16 @@
+package calculator.computation;
+
+public class NumberComponent implements EquationComponent {
+
+    private final String value;
+
+    public NumberComponent(final String value)
+    {
+        this.value=value;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+}
