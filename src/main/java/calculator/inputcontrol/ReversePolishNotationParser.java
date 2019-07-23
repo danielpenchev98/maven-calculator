18d59c01b0694b060f77882171bee9bf0e5f0c35
i515142 <daniel.penchev@sap.com>
2019-07-23 15:48:50 +0300

    Refactor ReversePolishNotationParser

--- src/main/java/calculator/inputcontrol/ReversePolishNotationParser.java
+++ src/main/java/calculator/inputcontrol/ReversePolishNotationParser.java
@@ -13,10 +13,9 @@ public class ReversePolishNotationParser {
 
     private List<EquationComponent> reversedPolishEquation;
 
-    public ReversePolishNotationParser()
-    {
-        operatorContainer=new Stack<>();
-        reversedPolishEquation=new LinkedList<>();
+    public ReversePolishNotationParser() {
+        operatorContainer = new Stack<>();
+        reversedPolishEquation = new LinkedList<>();
     }
 
     /**
@@ -29,99 +28,87 @@ public class ReversePolishNotationParser {
      * 3.2) if the operator on the top of the stack has lower or equal priority and is left associative, add current one to stack
      * 4) if it's closing bracket - add operators from the stack to equation, till you find opening bracket in the stack
      * 5) In the end add everything from the stack to equation
+     *
      * @param components infix notation
      * @return reverse polish notation
      */
-    public List<EquationComponent> formatFromInfixToReversedPolishNotation(final List<EquationComponent> components) throws EmptyStackException
-    {
-        for(EquationComponent component:components)
-        {
-           processComponent(component);
+    public List<EquationComponent> formatFromInfixToReversedPolishNotation(final List<EquationComponent> components) throws EmptyStackException {
+        for (EquationComponent component : components) {
+            processComponent(component);
         }
         addAllOperatorsLeftInTheContainerToEquation();
 
         return reversedPolishEquation;
     }
 
-    private void processComponent(final EquationComponent component)
-    {
-        if(component instanceof NumberComponent)
-        {
+    private void processComponent(final EquationComponent component) {
+        if (component instanceof NumberComponent) {
             addComponentToEquation(component);
-        }
-        else if (component instanceof MathArithmeticOperator)
-        {
+        } else if (component instanceof MathArithmeticOperator) {
             addOperatorsFromContainerDependingOnPriorityAndAssociativity((MathArithmeticOperator) component);
             addComponentToContainer(component);
-        }
-        else if(component instanceof ClosingBracket)
-        {
+        } else if (component instanceof ClosingBracket) {
             addOperatorsFromContainerToEquationTillOpeningBracketIsFound();
-        }
-        else
-        {
+        } else {
             addComponentToContainer(component);
         }
     }
 
-    private void addComponentToEquation(final EquationComponent component)
-    {
+    private void addComponentToEquation(final EquationComponent component) {
         reversedPolishEquation.add(component);
     }
 
-    private void addComponentToContainer(final EquationComponent component)
-    {
+    private void addComponentToContainer(final EquationComponent component) {
         operatorContainer.push(component);
     }
 
-    private boolean hasSpareOperators()
-    {
-        return operatorContainer.size()>0;
+    private boolean hasSpareOperators() {
+        return operatorContainer.size() > 0;
     }
 
-    private void addOperatorsFromContainerDependingOnPriorityAndAssociativity(final MathArithmeticOperator component)
-    {
-        while ( hasSpareOperators()&& shouldTransferOperatorsFromContainerToEquation(component)) {
+    private void addOperatorsFromContainerDependingOnPriorityAndAssociativity(final MathArithmeticOperator component) {
+        while (hasSpareOperators() && shouldTransferOperatorsFromContainerToEquation(component)) {
 
-            addComponentToEquation( operatorContainer.pop());
+            addComponentToEquation(operatorContainer.pop());
 
         }
     }
 
-    private boolean shouldTransferOperatorsFromContainerToEquation(final MathArithmeticOperator component)
-    {
-        if(operatorContainer.peek() instanceof OpeningBracket)
-        {
+    private boolean shouldTransferOperatorsFromContainerToEquation(final MathArithmeticOperator component) {
+        if (operatorContainer.peek() instanceof OpeningBracket) {
             return false;
         }
 
-        MathArithmeticOperator nextOperatorInContainer =(MathArithmeticOperator) operatorContainer.peek();
-        int differenceInPriority = calculateDifferenceInPriority(nextOperatorInContainer,component);
-        final boolean firstCondition = differenceInPriority<0;
-        final boolean secondCondition = differenceInPriority==0 && nextOperatorInContainer.isLeftAssociative();
+        MathArithmeticOperator nextOperatorInContainer = (MathArithmeticOperator) operatorContainer.peek();
+        int differenceInPriority = calculateDifferenceInPriority(nextOperatorInContainer, component);
+        final boolean firstCondition = differenceInPriority > 0;
+        final boolean secondCondition = differenceInPriority == 0 && nextOperatorInContainer.isLeftAssociative();
 
         return firstCondition || secondCondition;
     }
 
-    private int calculateDifferenceInPriority(final MathArithmeticOperator leftOperator,final MathArithmeticOperator rightOperator)
-    {
+    private int calculateDifferenceInPriority(final MathArithmeticOperator leftOperator, final MathArithmeticOperator rightOperator) {
         return leftOperator.getPriority() - rightOperator.getPriority();
     }
 
 
-    private void addOperatorsFromContainerToEquationTillOpeningBracketIsFound(){
+    private void addOperatorsFromContainerToEquationTillOpeningBracketIsFound() {
 
-        while(hasSpareOperators()&&!(operatorContainer.peek() instanceof OpeningBracket))
-        {
+        while (hasSpareOperators() && isNotOpeningBracket(operatorContainer.peek())) {
             addComponentToEquation(operatorContainer.pop());
         }
         operatorContainer.pop();
     }
 
+
+    private boolean isNotOpeningBracket(final EquationComponent component)
+    {
+        return !(component instanceof OpeningBracket);
+    }
+
     private void addAllOperatorsLeftInTheContainerToEquation() {
 
-        while( hasSpareOperators() )
-        {
+        while (hasSpareOperators()) {
             addComponentToEquation(operatorContainer.pop());
         }
     }
