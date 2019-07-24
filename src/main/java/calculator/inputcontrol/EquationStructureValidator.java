f12859ff197279788bf3fc91848f92481bc510b6
i515142 <daniel.penchev@sap.com>
2019-07-24 10:56:50 +0300

    Refactor EquationStructureValidator

--- src/main/java/calculator/inputcontrol/EquationStructureValidator.java
+++ src/main/java/calculator/inputcontrol/EquationStructureValidator.java
@@ -11,35 +11,23 @@ public class EquationStructureValidator {
     public void validateEquationStructure(final List<String> formattedEquation) throws InvalidEquationException
     {
         if(formattedEquation==null) {
-            throw new InvalidParameterException("validateEquation has received invalid parameter");
+           throw new InvalidParameterException("validateEquation has received invalid parameter");
         }
-        else if(formattedEquation.size()==0) {
-            throw new InvalidEquationException("Empty equation");
-        }
-        else if(hasBracketDisbalance(formattedEquation))
-        {
-            throw new InvalidEquationException("Equation with missing or misplaced brackets");
-        }
+        checkIfHasBracketImbalance(formattedEquation);
 
-        String equation= Arrays.toString(formattedEquation.toArray()).replaceAll("\\[|\\]|,","");
-        if(equation.matches(".*([0-9]+(\\.[0-9]+)? [0-9]+(\\.[0-9]+)?|.*[-+/*^] [-+/*^]).*"))
-        {
-            throw new InvalidEquationException("Sequential components of the same type");
-        }
-        else if(equation.matches("^[-+/*^].*|.*[-+/*^]$"))
-        {
-            throw new InvalidEquationException("Equation ending or beginning with operator");
-        }
-        else if(equation.matches(".*\\( \\).*"))
-        {
-            throw new InvalidEquationException("Empty brackets");
-        }
+        String equation=convertToWholeStringFormat(formattedEquation);
+
+        checkIfEmptyEquation(equation);
+        checkIfHasSequentialNonBracketComponentsOfTheSameType(equation);
+        checkIfHasAnOperatorAsBeginningOrEndingOfEquationScope(equation);
+        checkIfHasEmptyBrackets(equation);
+        checkIfHasMissingOperatorBetweenNumberAndBracket(equation);
     }
 
-
-    private boolean hasBracketDisbalance(final List<String> components)
+    private void checkIfHasBracketImbalance(final List<String> components) throws InvalidEquationException
     {
-        int bracketBalance=0;
+        int bracketBalance = 0;
+        boolean hasError = false;
         for(String component:components)
         {
             if(component.equals("("))
@@ -48,93 +36,87 @@ public class EquationStructureValidator {
             }
             else if(component.equals(")"))
             {
-                if(bracketBalance==0)
+                if(bracketBalance == 0)
                 {
-                    return true;
+                    hasError = true;
                 }
                 bracketBalance--;
             }
         }
-
-        return bracketBalance!=0;
-    }
-    /*public  void validateEquationStructure(final List<String> formattedEquation) throws InvalidEquationException
-    {
-        String message=null;
-        if(hasBracketDisbalance(formattedEquation))
+        if(hasError || bracketBalance != 0)
         {
-            message = "The equation is missing brackets";
-        }
-
-        String equation= Arrays.toString(formattedEquation.toArray()).replaceAll("\\[|\\]|,","");
-
-        if(equation.equals(""))
-        {
-            message = "Empty equation";
-        }
-        else if(hasSequentialTypeOfComponents(equation))
-        {
-            message = "Sequential operators has been found";
-        }
-        else if(hasNonOperatorBeforeOrAfterBracket(equation))
-        {
-            message = "There should be an operator between a number and an opening bracket or between a closing bracket and a number in that order";
-        }
-        else if(hasEmptyBrackets(equation))
-        {
-            message = "There should be at least one number in the brackets";
-        }
-        else if(hasOperatorAfterOpeningOrBeforeClosingBracket(equation))
-        {
-            message="There shouldn't be an operator before closing or after opening bracket";
-        }
-
-        if(message!=null)
-        {
-            throw new InvalidEquationException(message);
+            throw new InvalidEquationException("Equation with missing or misplaced brackets");
         }
     }
 
-    private boolean hasSequentialTypeOfComponents(final String equation)
+    private String convertToWholeStringFormat(final List<String> formattedEquation)
     {
-        return equation.matches(".*([^0-9.a-zA-Z() ]+[ ]*[^0-9.a-zA-Z() ]+|[0-9.a-zA-Z]+[ ]+[0-9.a-zA-Z]+).*");
+       return Arrays.toString(formattedEquation.toArray()).replaceAll("\\[|\\]|,","");
+    }
+
+    private void checkIfEmptyEquation(final String equation) throws InvalidEquationException
+    {
+        if(hasEmptyEquation(equation))
+        {
+            throw new InvalidEquationException("Empty equation");
+        }
+    }
+
+    private boolean hasEmptyEquation(final String equation)
+    {
+        return equation.matches("");
+    }
+
+    private void checkIfHasSequentialNonBracketComponentsOfTheSameType(final String equation) throws InvalidEquationException
+    {
+        if(hasSequentialNonBracketComponentsOfTheSameType(equation))
+        {
+            throw new InvalidEquationException("Sequential components of the same type");
+        }
+    }
+
+    private boolean hasSequentialNonBracketComponentsOfTheSameType(final String equation)
+    {
+        return equation.matches(".*([0-9]+(\\.[0-9]+)? [0-9]+(\\.[0-9]+)?|.*[-+/*^] [-+/*^]).*");
+    }
+
+    private void checkIfHasAnOperatorAsBeginningOrEndingOfEquationScope(final String equation) throws InvalidEquationException
+    {
+        if(hasAnOperatorAsBeginningOrEndingOfEquationScope(equation))
+        {
+            throw new InvalidEquationException("Scope of equation ending or beginning with an operator");
+        }
+    }
+
+    private boolean hasAnOperatorAsBeginningOrEndingOfEquationScope(final String equation)
+    {
+        return equation.matches("(^|.*\\( )[-+/*^] .*|.*[-+/*^]( \\)|$)");
+    }
+
+    private void checkIfHasEmptyBrackets(final String equation) throws InvalidEquationException
+    {
+        if(hasEmptyBrackets(equation))
+        {
+            throw new InvalidEquationException("Empty brackets");
+        }
     }
 
     private boolean hasEmptyBrackets(final String equation)
     {
-        return equation.matches(".*[(] [)].*");
+        return equation.matches(".*\\( \\).*");
     }
 
-    private boolean hasNonOperatorBeforeOrAfterBracket(final String equation)
+    private void checkIfHasMissingOperatorBetweenNumberAndBracket(final String equation) throws InvalidEquationException
     {
-        return equation.matches(".*[0-9.a-zA-Z)] [(].*|.*[)] [0-9.a-zA-Z(].*");
-    }
-
-    private boolean hasOperatorAfterOpeningOrBeforeClosingBracket(final String equation)
-    {
-        return equation.matches(".* [^0-9.a-zA-Z)(] [)].*|.*[(] [^0-9.a-zA-Z)(] .*");
-    }
-
-    private boolean hasBracketDisbalance(final List<String> equation)
-    {
-        int bracketBalance=0;
-
-        for(String item:equation)
+        if(hasMissingOperatorBetweenNumberAndBracket(equation))
         {
-            if(item.equals("("))
-            {
-                bracketBalance++;
-            }
-            else if(item.equals(")"))
-            {
-                if(bracketBalance==0)
-                {
-                    return true;
-                }
-                bracketBalance--;
-            }
+            throw new InvalidEquationException("Missing operator between a number and an opening bracket or a closing bracket and a number");
         }
-        return bracketBalance!=0;
-    }*/
+    }
+
+    private boolean hasMissingOperatorBetweenNumberAndBracket(final String equation)
+    {
+        return equation.matches(".*[^-+/*^(] \\(.*|.*\\) [^-+/*^)].*");
+    }
 
 }
--- src/test/java/calculator/inputcontrol/EquationStructureValidatorTest.java
+++ src/test/java/calculator/inputcontrol/EquationStructureValidatorTest.java
@@ -32,6 +32,14 @@ public class EquationStructureValidatorTest {
         validator.validateEquationStructure(input);
     }
 
+    @Test(expected = InvalidEquationException.class  )
+    public void validateEquation_EquationWithAnOperatorOnly_Illegal() throws Exception
+    {
+        List<String> input =new LinkedList<>();
+        input.add("+");
+        validator.validateEquationStructure(input);
+    }
+
     @Test
     public void validateEquationStructure_EquationWithANumberOnly_Legal() throws Exception
     {
@@ -78,133 +86,46 @@ public class EquationStructureValidatorTest {
     @Test(expected = InvalidEquationException.class)
     public void validateEquation_EquationWithMisplacedBracket_Illegal() throws Exception
     {
-        List<String> input=Arrays.asList("1","+",")","(");
+        List<String> input=Arrays.asList("(","1",")","(");
         validator.validateEquationStructure(input);
     }
 
     @Test(expected = InvalidEquationException.class)
-    public void validateEquation_EquationWithMissingBracket() throws Exception
+    public void validateEquation_EquationWithMissingBracket_Illegal() throws Exception
     {
         List<String> input=Arrays.asList("(","1","+","2");
         validator.validateEquationStructure(input);
     }
 
-   /* @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_EquationWithAnOperatorOnly_Illegal() throws Exception
-    {
-        List<String> input=new LinkedList<>();
-        input.add("+");
-        validator.validateEquationStructure(input);
-    }
-
     @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_NumberAndOperator_Illegal() throws Exception
-    {
-        List<String> input = Arrays.asList("2","+");
-        validator.validateEquationStructure(input);
-    }*/
-    /*
- //   private EquationStructureValidator validator;
-
-    @Before
-    public void setUp(){
-      validator=new EquationStructureValidator();
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void validateEquation_NullReference_Illegal() throws Exception
-    {
-        validator.validateEquationStructure(null);
-    }
-
-    @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_EquationWithMissingOperatorBetweenNumberAndBracket_Illegal() throws Exception {
-
-        List<String> input=new LinkedList<>(Arrays.asList("2","(","-5",")"));
-        input.add("2");
-        input.add("(");
-        input.add("-5");
-        input.add(")");
-
-        validator.validateEquationStructure(input);
-    }
-
-    @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_EquationWithClosingBracketNextToOpeningBracket_Legal() throws Exception
-    {
-        List<String> input=Arrays.asList("(","(","10","+","20",")","(","-5",")",")");
-        validator.validateEquationStructure(input);
-    }
-
-    @Test
-    public void validateEquationStructure_EquationWithOnlyANumber_Legal() throws Exception
-    {
-        List<String> input=new LinkedList<>();
-        input.add("1");
-        validator.validateEquationStructure(input);
-    }
-
-    @Test
-    public void validateEquationStructure_SimpleEquation_Illegal() throws Exception {
-        List<String> input=Arrays.asList("(","(","-1","+","5","+","2",")",")");
-        validator.validateEquationStructure(input);
-    }
-
-    @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_EquationOnlyWithOperatorInTheBrackets_Illegal() throws Exception
-    {
-        List<String> input=Arrays.asList("(","+",")","+","1");
-        validator.validateEquationStructure(input);
-    }
-
-    @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_EquationWithSequentialOperators_Illegal() throws Exception
-    {
-        List<String> input=Arrays.asList("2","+","+","1");
-        validator.validateEquationStructure(input);
-    }
-
-    @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_EquationWithEmptyBrackets_Illegal() throws Exception
-    {
-        List<String> input=Arrays.asList("(",")","+","2");
-        validator.validateEquationStructure(input);
-    }
-
-    @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_EmptyEquation_Illegal() throws Exception
-    {
-        List<String> input=new LinkedList<>();
-        input.add("");
-        validator.validateEquationStructure(input);
-    }
-
-    @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_EquationWithMissingBracket_Illegal() throws Exception
-    {
-        List<String> input=Arrays.asList("(","(","2.0","+","1",")");
-        validator.validateEquationStructure(input);
-    }
-
-    @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_OperatorAfterOpeningBracket_Illegal() throws Exception
-    {
-        List<String> input=Arrays.asList("(","+","1",")");
-        validator.validateEquationStructure(input);
-    }
-
-    @Test(expected = InvalidEquationException.class)
-    public void validateEquationStructure_OperatorBeforeClosingBracket_Illegal() throws Exception
+    public void validateEquation_EquationWithOperatorBeforeClosingBracket_Illegal() throws Exception
     {
         List<String> input=Arrays.asList("(","1","+",")");
         validator.validateEquationStructure(input);
     }
 
     @Test(expected = InvalidEquationException.class)
-    public void validateEquationStrucure_EquationEndingWithOperator_Illegal() throws Exception
+    public void validateEquation_EquationWithMissingOperatorBetweenNumberAndBracket_Illegal() throws Exception
     {
-        List<String> input=Arrays.asList("1","+");
+        List<String> input=Arrays.asList("1","(","2",")");
         validator.validateEquationStructure(input);
-    }*/
+    }
+
+    @Test(expected = InvalidEquationException.class)
+    public void validateEquation_EquationWithMissingOperatorBetweenClosingAndOpeningBracket_Illegal() throws Exception
+    {
+        List<String> input=Arrays.asList("(","1",")","(","1",")");
+        validator.validateEquationStructure(input);
+
+    }
+
+    @Test
+    public void validateEquation_EquationWithMultipleLayersOfBrackets_Legal() throws Exception
+    {
+        List<String> input=Arrays.asList("(","(","1","+","2",")",")");
+        validator.validateEquationStructure(input);
+    }
+
+
 
 }
