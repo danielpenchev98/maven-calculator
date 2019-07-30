ee578e23868fec1bfabe389962a9fc853e247cfa
danielpenchev98 <daniel.penchev@sap.com>
2019-07-30 17:58:16 +0300

    Add CalculatorConsole

--- calculator-servlet/src/main/java/webapp/CalculateServlet.java
+++ calculator-servlet/src/main/java/webapp/CalculateServlet.java
@@ -1,6 +1,5 @@
 package webapp;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
