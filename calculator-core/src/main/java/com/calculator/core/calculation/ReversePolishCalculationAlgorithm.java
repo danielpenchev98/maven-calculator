8a5aa8916114ebbc99a2e7dec7892d87f3cc4ada
danielpenchev98 <daniel.penchev@sap.com>
2019-08-05 15:36:31 +0300

    Redo reformatting of ReversePolishCalculationAlgorithm

--- calculator-core/src/main/java/com/calculator/core/calculation/ReversePolishCalculationAlgorithm.java
+++ calculator-core/src/main/java/com/calculator/core/calculation/ReversePolishCalculationAlgorithm.java
@@ -12,66 +12,62 @@ import java.util.Stack;
 
 public class ReversePolishCalculationAlgorithm implements CalculationAlgorithm {
 
+    private Stack<NumberComponent> supplier;
+
     private final ReversePolishNotationParser parser;
 
     private final int NUMBERS_IN_SUPPLIER_AFTER_CALCULATION = 1;
 
-    public ReversePolishCalculationAlgorithm()
-    {
+    public ReversePolishCalculationAlgorithm() {
         this(new ReversePolishNotationParser());
     }
 
-    public ReversePolishCalculationAlgorithm(final ReversePolishNotationParser parser)
-    {
-        this.parser=parser;
+    public ReversePolishCalculationAlgorithm(final ReversePolishNotationParser parser) {
+        this.parser = parser;
+        this.supplier=new Stack<>();
     }
+
 
     public double calculateEquation(final List<EquationComponent> equation) throws EmptyStackException, InvalidEquationException {
-
-        List<EquationComponent> equationInRPN=formatToReversePolishNotation(equation);
-        Stack<NumberComponent> numberSupplier=new Stack<>();
+        List<EquationComponent> equationInRPN = formatToReversePolishNotation(equation);
 
         for (EquationComponent component : equationInRPN) {
-            process(component,numberSupplier);
+            process(component);
         }
 
-        if (hasErrorInTheCalculation(numberSupplier)) {
+        if (hasErrorInTheCalculation()) {
             throw new InvalidEquationException("Invalid equation. Logical error. There aren't enough operators");
         }
-
-        return getNextNumber(numberSupplier);
+        return getNextNumberFromSupplier();
     }
 
-    private List<EquationComponent> formatToReversePolishNotation(final List<EquationComponent> equation)
-    {
+    private List<EquationComponent> formatToReversePolishNotation(final List<EquationComponent> equation) {
         return parser.formatFromInfixToReversedPolishNotation(equation);
     }
 
-    private void process(final EquationComponent component,final Stack<NumberComponent> numberSupplier) {
+    private void process(final EquationComponent component) {
         if (component instanceof NumberComponent) {
-            numberSupplier.add((NumberComponent) component);
+            supplier.add((NumberComponent) component);
         } else if (component instanceof MathArithmeticOperator) {
-            executeOperation((MathArithmeticOperator) component,numberSupplier);
+            executeOperation((MathArithmeticOperator) component);
         } else {
             throw new InvalidParameterException("Error. There shouldn't be any components different from numbers and math arithmetic operators");
         }
     }
 
-    private void executeOperation(final MathArithmeticOperator operator,final Stack<NumberComponent> supplier) throws EmptyStackException {
-        double rightNumber = getNextNumber(supplier);
-        double leftNumber = getNextNumber(supplier);
+    private void executeOperation(final MathArithmeticOperator operator) throws EmptyStackException {
+        double rightNumber = getNextNumberFromSupplier();
+        double leftNumber = getNextNumberFromSupplier();
         double result = operator.compute(leftNumber, rightNumber);
         supplier.add(new NumberComponent(String.valueOf(result)));
     }
 
-    private double getNextNumber(final Stack<NumberComponent> supplier) {
+    private double getNextNumberFromSupplier() {
         String number = supplier.pop().getValue();
         return Double.parseDouble(number);
     }
 
-    private boolean hasErrorInTheCalculation(final Stack<NumberComponent> supplier)
-    {
-        return supplier.size()!=NUMBERS_IN_SUPPLIER_AFTER_CALCULATION;
+    private boolean hasErrorInTheCalculation() {
+        return supplier.size() != NUMBERS_IN_SUPPLIER_AFTER_CALCULATION;
     }
-
 }
