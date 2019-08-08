ed055584b1babbaefa47b39cf46f154f72893771
dspenchev <danielpenchev@abv.bg>
2019-08-09 00:37:13 +0300

    Add logger in CalculatorConsole

--- calculator-console/src/main/java/com/calculator/console/CalculatorConsole.java
+++ calculator-console/src/main/java/com/calculator/console/CalculatorConsole.java
@@ -7,6 +7,8 @@ import com.calculator.core.exceptions.InvalidEquationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.logging.FileHandler;
+
 public class CalculatorConsole {
 
         private static final int VALID_NUMBER_OF_ARGUMENTS=1;
@@ -33,12 +35,26 @@ public class CalculatorConsole {
                 System.out.print("Arithmetic error :" + ex.getMessage());
             } catch (Exception sysError) {
 
-                logger.error("Problem with the application",sysError);
+                createErrorLog(sysError);
                 System.out.print("Problem with the application");
             }
         }
 
         private static boolean hasInvalidNumberOfArguments(final int argLength) {
             return VALID_NUMBER_OF_ARGUMENTS != argLength;
+        }
+
+        private static void createErrorLog(Exception sysError)
+        {
+            /*try
+            {
+            FileHandler fh=new FileHandler("./errorLog.log",true);
+            logger.addHandler(fh);
+            logger.error("Problem with the application");
+            }
+            catch(Exception ex)
+            {
+
+            }*/
         }
 }
