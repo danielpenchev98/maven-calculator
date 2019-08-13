86be0e0de7d91380810eddfa47cb0bb1e809af01
danielpenchev98 <daniel.penchev@sap.com>
2019-08-13 13:10:39 +0300

    Fix MainPage logic of creating an URL

--- calculator-servlet-test/src/test/java/com/calculator/webapp/test/webclient/MainPage.java
+++ calculator-servlet-test/src/test/java/com/calculator/webapp/test/webclient/MainPage.java
@@ -19,7 +19,8 @@ public class MainPage {
 
     private static final String ENCODING = "UTF-8";
     private static final String CALCULATOR_SERVLET_URL = "/calculator";
-    private static final String GET_REQUEST_URL = "/api/v1/calculation/";
+    private static final String GET_REQUEST_URL = "/api/v1/calculation";
+    private static final String REQUEST_PARAMETER="equation";
 
     public MainPage(final URL baseUrl) {
         this.baseUrl = baseUrl;
@@ -37,8 +38,7 @@ public class MainPage {
 
     private URL getEncodedUrl(String unformattedInput) throws IOException {
         String spec = URLEncoder.encode(unformattedInput, ENCODING);
-        URI temp= URI.create(baseUrl+CALCULATOR_SERVLET_URL + GET_REQUEST_URL + spec);
-        return temp.toURL();
+        return new URL(baseUrl,CALCULATOR_SERVLET_URL + GET_REQUEST_URL +"?"+REQUEST_PARAMETER+"="+spec);
     }
 
     private void openConnection(final URL url) throws IOException {
