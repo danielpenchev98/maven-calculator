6872d67ae0f3e805ff59e37b953c88e0bc4f7f79
danielpenchev98 <daniel.penchev@sap.com>
2019-08-21 16:47:31 +0300

    Refactor EquationComponentFactory to work with HashMap

--- calculator-core/src/main/java/com/calculator/core/operators/EquationComponentFactory.java
+++ calculator-core/src/main/java/com/calculator/core/operators/EquationComponentFactory.java
@@ -5,24 +5,39 @@ import com.calculator.core.exceptions.InvalidComponentException;
 public class EquationComponentFactory {
 
     private final static String validNumberRegex = "-?[0-9]+(.[0-9]+)?";
+    private final static Map<String,EquationComponent> validOperators = new HashMap<>() {{
+                                                                            put(OpeningBracket.SYMBOL, new OpeningBracket());
+                                                                            put(ClosingBracket.SYMBOL, new ClosingBracket());
+                                                                            put(Addition.SYMBOL, new Addition());
+                                                                            put(Subtraction.SYMBOL, new Subtraction());
+                                                                            put(Multiplication.SYMBOL, new Multiplication());
+                                                                            put(Division.SYMBOL, new Division());
+                                                                            put(Power.SYMBOL, new Power());
+                                                                        }};
 
-    public EquationComponent createComponent(final String operation) throws InvalidComponentException
+    public EquationComponent createComponent(final String component) throws InvalidComponentException
     {
-        if(operation.matches(validNumberRegex))
+        if(isValidNumber(component))
         {
-            return new NumberComponent(operation);
+            return new NumberComponent(component);
         }
+        else if(isValidOperator(component))
+        {
+            return validOperators.get(component);
+        }
+        else
+        {
+            throw new InvalidComponentException("Unsupported component :"+operation);
+        }
+    }
 
-        switch(operation)
-        {
-            case OpeningBracket.SYMBOL: return new OpeningBracket();
-            case ClosingBracket.SYMBOL: return new ClosingBracket();
-            case Addition.SYMBOL: return new Addition();
-            case Subtraction.SYMBOL:return new Subtraction();
-            case Multiplication.SYMBOL: return new Multiplication();
-            case Division.SYMBOL:return new Division();
-            case Power.SYMBOL:return new Power();
-            default: throw new InvalidComponentException("Unsupported component :"+operation);
-        }
+    private boolean isValidNumber(final String component)
+    {
+        return component.matches(validNumberRegex);
+    }
+
+    private boolean isValidOperator(final String component)
+    {
+        return validOperators.containsKey(component);
     }
 }
