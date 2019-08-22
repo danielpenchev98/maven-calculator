6ebe76ef5dd247ad585079e53b173234e70d1299
danielpenchev98 <daniel.penchev@sap.com>
2019-08-22 16:36:48 +0300

    Create asynch request to REST service

--- calculator-web-ui/webapp/controller/CalculatorPanel.controller.js
+++ calculator-web-ui/webapp/controller/CalculatorPanel.controller.js
@@ -1,13 +1,22 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/ui/model/json/JSONModel",
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("com.calculator.web.ui.controller.CalculatorPanel", {
 
 		onOpenDialog : function () {
-			this.getOwnerComponent().openResultDialog();
+
+			let url="http://localhost:8080/api/v1/calculate?equation=2%2a2";
+			let oModel=new JSONModel();
+			let ownerComponent=this.getOwnerComponent();
+			oModel.loadData(url).then(function(){
+				ownerComponent.openResultDialog(oModel);
+			}).catch(function(error){
+				oModel.setJSON(error.responseText);
+				ownerComponent.openErrorDialog(oModel);
+			});
 		}
 	});
 
