816cc4aa667c408fbf526a28f44fc3f30ec7d620
dspenchev <danielpenchev@abv.bg>
2019-09-01 00:50:42 +0300

    Add neo-java-web-maven-plugin to connect with Neo and cf-maven-plugin to connect to cloud foundry

--- calculator-web-ui/src/main/webapp/test/server/server.js
+++ calculator-web-ui/src/main/webapp/test/server/server.js
@@ -13,7 +13,7 @@ sap.ui.define(
         let okCode = 200;
 
         //change the baseUrl in case the domain changes
-        let baseUrl="http://localhost:7777";
+        let baseUrl="https://calculato-web-service.cfapps.eu10.hana.ondemand.com";
         let serviceUrl="/api/v1/calculate";
         let queryParam="equation";
         let urlWithoutQueryParamValue = baseUrl + serviceUrl + '?' + queryParam + '=';
