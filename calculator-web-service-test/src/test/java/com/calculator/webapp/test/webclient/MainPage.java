79717951718078a3bcfc4591a7d44dcc9e505380
danielpenchev98 <daniel.penchev@sap.com>
2019-09-02 18:20:09 +0300

    Change the name of the variable calculator_servlet_url to calculator_service_war

--- calculator-web-service-test/src/test/java/com/calculator/webapp/test/webclient/MainPage.java
+++ calculator-web-service-test/src/test/java/com/calculator/webapp/test/webclient/MainPage.java
@@ -16,7 +16,7 @@ public class MainPage {
     private final URL baseUrl;
 
     private static final String ENCODING = "UTF-8";
-    private static final String CALCULATOR_SERVLET_URL = "/calculator";
+    private static final String CALCULATOR_SERVICE_WAR = "/calculator";
     private static final String GET_REQUEST_URL = "/api/v1/calculate";
     private static final String REQUEST_PARAMETER="equation";
 
@@ -33,6 +33,6 @@ public class MainPage {
 
     private URL getEncodedUrl(final String unformattedInput) throws IOException {
         String encodedEquation = URLEncoder.encode(unformattedInput, ENCODING);
-        return new URL(baseUrl,CALCULATOR_SERVLET_URL + GET_REQUEST_URL +"?"+REQUEST_PARAMETER+"="+encodedEquation);
+        return new URL(baseUrl,CALCULATOR_SERVICE_WAR + GET_REQUEST_URL +"?"+REQUEST_PARAMETER+"="+encodedEquation);
     }
 }
