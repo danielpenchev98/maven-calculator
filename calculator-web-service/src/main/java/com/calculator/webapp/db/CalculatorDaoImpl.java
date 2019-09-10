78c16d15d9847bb05b8e078410018cce220d1047
danielpenchev98 <daniel.penchev@sap.com>
2019-09-10 18:36:12 +0300

    Add functionality to Dao object

--- calculator-web-service/src/main/java/com/calculator/webapp/db/CalculatorDaoImpl.java
+++ calculator-web-service/src/main/java/com/calculator/webapp/db/CalculatorDaoImpl.java
@@ -1,25 +1,31 @@
 package com.calculator.webapp.db;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 public class CalculatorDaoImpl implements Dao<Long,CalculatorResponseDTO> {
 
     private EntityManager manager;
 
+    @Inject
     public CalculatorDaoImpl(final EntityManager manager) {
         this.manager = manager;
     }
 
-
+    @SuppressWarnings("JpaQueryApiInspection")
     @Override
-    public List<CalculatorResponseDTO> getAllItems() {
-        return null;
+    public List<CalculatorResponseDTO> getAllItems()  {
+        Query query=manager.createNamedQuery("CalculatorResponses.findAll",CalculatorResponseDTO.class);
+        return query.getResultList();
     }
 
     @Override
     public CalculatorResponseDTO getItem(Long key) {
-        return null;
+        CalculatorResponseDTO result = manager.find(CalculatorResponseDTO.class,key);
+        manager.detach(result);
+        return result;
     }
 
     @Override
--- calculator-web-service/src/test/java/com/calculator/webapp/db/CalculatorDaoImplTest.java
+++ calculator-web-service/src/test/java/com/calculator/webapp/db/CalculatorDaoImplTest.java
@@ -1,79 +1,90 @@
 package com.calculator.webapp.db;
 
-
+import org.dbunit.DatabaseUnitException;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
-import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.dataset.ITable;
 import org.dbunit.operation.DatabaseOperation;
 import org.dbunit.util.fileloader.FlatXmlDataFileLoader;
+import org.hamcrest.Matchers;
 import org.junit.*;
-import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import javax.persistence.*;
+import java.text.ParseException;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class CalculatorDaoImplTest {
 
-    @PersistenceContext(name="test-unit")
-    private EntityManager manager;
-
-    @Inject
-    private CalculatorDaoImpl dao;
+    private static EntityManager manager;
+    private final CalculatorDaoImpl dao = new CalculatorDaoImpl(manager);
     private static IDatabaseTester databaseTester;
 
     @BeforeClass
     public static void setUpDB() throws Exception {
-        //Connection connection = DriverManager.getConnection("jdbc:derby:memory://127.0.0.1:1527/calculator;create=true","root","root");
         databaseTester = new JdbcDatabaseTester("org.apache.derby.jdbc.EmbeddedDriver",
                 "jdbc:derby:memory://127.0.0.1:1527/calculator;create=true","root","root");
+        EntityManagerFactory factory= Persistence.createEntityManagerFactory("test-unit");
+         manager=factory.createEntityManager();
     }
 
     @AfterClass
     public static void tearDownDB() throws Exception {
+        manager.close();
         databaseTester.getConnection().close();
     }
 
-    @Before
-    public void setUp() throws Exception
-    {
-        //dao=new CalculatorDaoImpl(manager);
-
-        //IDataSet populatedDataSet=new FlatXmlDataSetBuilder().build(getClass().getResourceAsStream("/datasets/populatedDataSet.xml"));
-        IDataSet populatedDataSet = new FlatXmlDataFileLoader().load("/datasets/populatedDataSet.xml");
-        databaseTester.setDataSet(populatedDataSet);
-        DatabaseOperation.CLEAN_INSERT.execute(databaseTester.getConnection(),populatedDataSet);
-    }
-
     @Test
-    public void getAllItems_populatedDataSet_() {
-        //IDataSet expectedDataSet = new FlatXmlDataFileLoader().load("populatedDataSet.xml");
+    public void getAllItems_populatedDataSet_expectedSize() throws Exception {
+        IDataSet initialDataSet = getDataSet("/datasets/populatedDataSet.xml");
+        uploadDataSet(initialDataSet);
+
+        final int expectedNumberOfEntities = 6;
         List<CalculatorResponseDTO> actualItems = dao.getAllItems();
-        assertEquals(2,actualItems.size());
+
+        assertThat(actualItems.size(),is(equalTo(expectedNumberOfEntities)));
+    }
+
+
+    //should all properties be tested?
+    @Test
+    public void getItem_populatedDataSet_expectedItem() throws Exception {
+        IDataSet initialDataSet = getDataSet("/datasets/populatedDataSet.xml");
+        uploadDataSet(initialDataSet);
+
+        CalculatorResponseDTO actualItem=dao.getItem(6L);
+
+        assertThat(actualItem,is(notNullValue()));
+        assertThat(actualItem.getId(),is(equalTo(6L)));
     }
 
     @Ignore
     @Test
-    public void getItem() {
-        assert(true);
-    }
+    public void saveItem_emptyDataSet_tableSize1() {
+        CalculatorResponseDTO response = new CalculatorResponseDTO();
+        response.setLegitimacy(false);
+        response.setEquation("1");
+        response.setResponseMsg("1.0");
 
-    @Ignore
-    @Test
-    public void saveItem() {
-        //CalculatorResponseDTO toSaveEntity = new CalculatorResponseDTO(true,"10/2","5.0");
-        //CalculatorDaoImpl
+        dao.saveItem(response);
     }
 
     @Ignore
     @Test
     public void deleteItem() {
+
+    }
+
+    IDataSet getDataSet(final String path)
+    {
+        return new FlatXmlDataFileLoader().load(path);
+    }
+
+    void uploadDataSet(final IDataSet wantedDataSet) throws Exception {
+        databaseTester.setDataSet(wantedDataSet);
+        DatabaseOperation.CLEAN_INSERT.execute(databaseTester.getConnection(),wantedDataSet);
     }
 }
