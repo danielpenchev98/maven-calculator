dc35c19000b2fe8278611f9f84820519ec161d6e
danielpenchev98 <daniel.penchev@sap.com>
2019-10-04 13:51:33 +0300

    Use jersey-jackson for serialization of DTO in CalculatorRestResource

--- calculator-web-service/src/main/java/com/calculator/webapp/injectionconfig/CalculatorWeb.java
+++ calculator-web-service/src/main/java/com/calculator/webapp/injectionconfig/CalculatorWeb.java
@@ -1,11 +1,13 @@
 package com.calculator.webapp.injectionconfig;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class CalculatorWeb extends ResourceConfig {
     public CalculatorWeb() {
         //specifying the custom binder
         register(new CalculatorWebBinder());
+        register(JacksonFeature.class);
         //specifying the location of the Rest resource
         packages(true,"com.calculator.webapp.restresources");
     }
--- calculator-web-service/src/main/java/com/calculator/webapp/restresponse/CalculationResult.java
+++ calculator-web-service/src/main/java/com/calculator/webapp/restresponse/CalculationResult.java
@@ -1,18 +1,24 @@
 package com.calculator.webapp.restresponse;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
 public class CalculationResult {
     private String result;
 
-    public CalculationResult(final String result)
-    {
+    @JsonCreator
+    public CalculationResult(@JsonProperty("result") final String result) {
         setResult(result);
     }
 
-    public void setResult(final String result)
-    {
+    @JsonSetter("result")
+    public void setResult(final String result) {
         this.result=result;
     }
 
+    @JsonGetter("result")
     public String getResult() {
         return this.result;
     }
