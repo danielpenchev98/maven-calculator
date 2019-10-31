10cf4f1cca1cb17ffaafef9b6586c242037454a3
danielpenchev98 <daniel.penchev@sap.com>
2019-10-31 09:34:06 +0200

    Replace jquery.ajax with xmlHttpRequests

--- calculator-web-ui/src/main/webapp/controller/CalculatorPanel.controller.js
+++ calculator-web-ui/src/main/webapp/controller/CalculatorPanel.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
+	"sap/ui/model/json/JSONModel",
 ], function (Controller, JSONModel) {
 	"use strict";
 
@@ -16,82 +16,69 @@ sap.ui.define([
 			this.sendCalculationRequest(oServiceConfig, equation);
 		},
 
-		sendCalculationRequest : async function (oServiceConfig,equation) {
+
+		doXMLHttpRequest:function(xhr,method,url,onloadEventListener,data){
+			xhr.open(method,url);
+			xhr.setRequestHeader("Content-Type","application/json");
+			xhr.setRequestHeader("cache-control","no-cache");
+			xhr.addEventListener("load", onloadEventListener);
+			xhr.send(data);
+		},
+
+		sendCalculationRequest : function (oServiceConfig,equation) {
 			let postRequestUrl = oServiceConfig.baseUrl + oServiceConfig.sendCalculationRequestUrl;
 
 			let postRequestBody = {
 				equation : equation
 			};
 
-            let settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": postRequestUrl,
-                "method": "POST",
-                "headers": {
-                    "Content-Type": "application/json",
-                    "Accept": "*/*",
-                    "Cache-Control": "no-cache",
-                    "cache-control": "no-cache"
-                },
-				"data": JSON.stringify(postRequestBody)
-			};
-
-			$.ajax(settings).done(function(data){
-				console.log("id object ->"+data);
-				const processId=setInterval(()=> {
-					this.getCalculationResult(oServiceConfig, data).then(completed => {
-						console.log("check if finished ->"+completed);
-						if(completed) {
-							console.log("stop the interval");
-							clearInterval(processId);
-						}
-					});
-				},4000);
-			}.bind(this))
-				.fail(function(jqXhr){
-					     this.showCalculationError(jqXhr.responseText);
-				      }.bind(this)
-				);
+			let xhr = new XMLHttpRequest();
+			let onloadEventListener = function(){
+				this.onCalculationRequestQueued(xhr,oServiceConfig);
+			}.bind(this);
+			this.doXMLHttpRequest(xhr,"POST",postRequestUrl,onloadEventListener,JSON.stringify(postRequestBody));
 		},
 
-		getCalculationResult : async function (oServiceConfig,oId) {
-			let getRequestUrl = oServiceConfig.baseUrl + oServiceConfig.getCalculationResultUrl + "/" + oId.id;
+		onCalculationRequestQueued : function(xhr,oServiceConfig){
+			if(this.isRequestDone(xhr.readyState)) {
+				let oId = JSON.parse(xhr.response);
+				let processId = setInterval(() => {
+					this.getCalculationResult(oServiceConfig, oId, processId)
+				}, 1000);
+			}
+		},
 
-			let settings = {
-				"async": true,
-				"crossDomain": true,
-				"url": getRequestUrl,
-				"method": "GET",
-				"headers": {
-					"Content-Type": "application/json",
-					"cache-control": "no-cache",
-					"Accept": "*/*"
-				}
-			};
 
-			let hasFinished = false;
+		getCalculationResult : function (oServiceConfig,oId,processId) {
+			let getCalculationResultUrl = oServiceConfig.baseUrl + oServiceConfig.getCalculationResultUrl + "/" + oId.id;
 
-			//try {
-			await $.ajax(settings).done(function (data, textStatus, jQxhr) {
-				if (jQxhr.status === 200) {
-					console.log("result");
-					this.showCalculationResult(data);
-					hasFinished = true;
-					console.log("result showed");
-				}
-			}.bind(this)).fail(function (jqXHR) {
-				console.log("error " + jqXHR.responseJSON);
-				//this.showCalculationError(jqXHR.responseJSON);
-				console.log("error showed");
-				hasFinished = true;
-			}.bind(this));
-		//}
-		//	catch(exception){
-		//		console.log(exception);
-		//	}
-			console.log("returning the flag");
-			return hasFinished;
+			let xhr = new XMLHttpRequest();
+			let onloadEventListener = function() {
+				this.onCalculationResultCompleted(xhr, processId);
+			}.bind(this);
+			this.doXMLHttpRequest(xhr,"GET",getCalculationResultUrl,onloadEventListener,null);
+
+		},
+
+		onCalculationResultCompleted : function(xhr,processId) {
+		   if(this.isRequestDone(xhr.readyState)&&this.isCompletedCalculation(xhr.status)){
+		   	    let oResponse = JSON.parse(xhr.response);
+		   	    console.log(oResponse);
+		   	    this.isSuccessfulCalculation(xhr.status) ? this.showCalculationResult(oResponse) : this.showCalculationError(oResponse);
+			    clearInterval(processId);
+		   }
+		},
+
+		isRequestDone : function(readyState){
+			return readyState === 4;
+		},
+
+		isCompletedCalculation : function(statusCode){
+			return statusCode !== 202;
+		},
+
+		isSuccessfulCalculation : function(statusCode) {
+			return statusCode === 200;
 		},
 
 		showCalculationResult : function (CalculationResult) {
@@ -102,7 +89,7 @@ sap.ui.define([
 
 		showCalculationError : function (errorMessage) {
 			let mError = new JSONModel();
-			mError.setJSON(errorMessage);
+			mError.setJSON(JSON.stringify(errorMessage));
 			this.getOwnerComponent().openErrorDialog(mError);
 		}
 	});
