8b65ba67d5cfd10fcb2de95e3bc0488760b1b1b0
danielpenchev98 <daniel.penchev@sap.com>
2019-11-12 16:04:07 +0200

    Refactor CalculationsTable - add EventBus, remove setInterval and xmlHttpRequests

--- calculator-web-ui/src/main/webapp/controller/CalculationsTable.controller.js
+++ calculator-web-ui/src/main/webapp/controller/CalculationsTable.controller.js
@@ -1,106 +1,58 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-], function (Controller) {
+    "sap/ui/model/json/JSONModel"
+], function (Controller,JSONModel) {
     "use strict";
 
     return Controller.extend("com.calculator.web.ui.controller.CalculationsTable", {
 
+        calculationHistoryKeyInSessionStorage : "calculationHistory",
+
+        calculationHistory : {},
 
         onInit: function () {
-
-            let ownerComponent = this.getOwnerComponent();
-            let oServiceConfig = ownerComponent.getManifestEntry("/sap.app/dataSources/calculationService");
-
-            let getHistoryUrl = oServiceConfig.baseUrl + oServiceConfig.getCalculationHistoryUrl;
-
-            setInterval(() => {
-                console.log(sessionStorage.getItem("calculationHistory"));
-                let xhr = new XMLHttpRequest();
-                let onloadEventListener = function () {
-                    this.onCalculationHistoryReceived(xhr);
-                }.bind(this);
-                this.doXMLHttpRequest(xhr, "GET", getHistoryUrl, onloadEventListener, null);
-            }, 3000);
+            let eventBus = sap.ui.getCore().getEventBus();
+            eventBus.subscribe("updateChannel", "completedCalculation", this.onCompletedCalculation, this);
+            eventBus.subscribe("updateChannel", "pendingCalculation", this.onNewRecord, this);
         },
 
-        onCalculationHistoryReceived: function (xhr) {
-            let historyTable = this.getView().byId("calculationsTable");
-            let oModel = new sap.ui.model.json.JSONModel();
-
-            let history = JSON.parse(xhr.responseText);
-
-            history.forEach((calculation) => {
-                if(this.isPendingCalculation(calculation)) {
-                    calculation.errorMsg="Pending calculation";
-                }
-
-                if (this.hasCalculationError(calculation)){
-                    calculation.calculationResult=null;
-                }
-            });
-
-            oModel.setData(history);
-            historyTable.setModel(oModel);
+        onNewRecord : function(ChannelId, EventId, data){
+            this.updateAppData(this.createNewPendingCalculationInHistory.bind(this),data);
         },
 
-        /* onInit: function () {
-
-            let ownerComponent = this.getOwnerComponent();
-            let oServiceConfig = ownerComponent.getManifestEntry("/sap.app/dataSources/calculationService");
-
-            let getResultUrl = oServiceConfig.baseUrl + oServiceConfig.getCalculationResultUrl;
-
-            let historyTable = this.getView().byId("calculationsTable");
-
-            setInterval(() => {
-                let calculationHistory = sessionStorage.getItem("calculationHistory");
-                calculationHistory.forEach(function(calculationRecord){
-                    let xhr = new XMLHttpRequest();
-                    let onloadEventListener = function () {
-                        this.onCalculationHistoryReceived(xhr);
-                    }.bind(this);
-                    this.doXMLHttpRequest(xhr, "GET",getResultUrl+"/"+calculationRecord.id, onloadEventListener, null);
-                });
-                let oModel = new sap.ui.model.json.JSONModel();
-                oModel.set(this.calculationHistory);
-                historyTable.setModel(oModel);
-            }, 3000);
+        onCompletedCalculation : function(ChannelId, EventId, data){
+            this.updateAppData(this.updateCalculationResult.bind(this),data);
         },
 
-        onCalculationHistoryReceived: function (xhr) {
-
-            if(xhr.status==202){
-
-            }
-            let calculationRecord = JSON.parse(xhr.responseText);
-
-            if(calculationRecord.)
-                if(this.isPendingCalculation(calculation)) {
-                    calculation.errorMsg="Pending calculation";
-                }
-
-                if (this.hasCalculationError(calculation)){
-                    calculation.calculationResult=null;
-                }
-
-        },*/
-
-        isPendingCalculation:function(calculation){
-          return  calculation.statusCode === 0
+        updateAppData : function(action,data){
+            this.updateCalculationHistoryFromSessionStorage();
+            action(data);
+            this.updateTable();
+            this.updateSessionStorage();
         },
 
-
-        hasCalculationError : function(calculation){
-            return calculation.errorMsg!==null;
+        updateTable : function(){
+            let oModel = new JSONModel();
+            oModel.setData(this.calculationHistory);
+            this.getView().setModel(oModel);
         },
 
-        doXMLHttpRequest:function(xhr,method,url,onloadEventListener,data){
-            xhr.open(method,url);
-            xhr.setRequestHeader("Content-Type","application/json");
-            xhr.setRequestHeader("cache-control","no-cache");
-            xhr.addEventListener("load", onloadEventListener);
-            xhr.send(data);
+        createNewPendingCalculationInHistory : function(data){
+            this.calculationHistory[data.id]= { equation : data.equation, result : "Pending calculation"};
         },
+
+        updateCalculationResult : function(data){
+            this.calculationHistory[data.id].result=data.result;
+        },
+
+        updateCalculationHistoryFromSessionStorage:function() {
+            let calculationHistoryJSONString = sessionStorage.getItem(this.calculationHistoryKeyInSessionStorage);
+            this.calculationHistory = calculationHistoryJSONString !== null ? JSON.parse(calculationHistoryJSONString) : {};
+        },
+
+        updateSessionStorage:function(){
+            sessionStorage.setItem(this.calculationHistoryKeyInSessionStorage,JSON.stringify(this.calculationHistory));
+        }
     });
 
 });
